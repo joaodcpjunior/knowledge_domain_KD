960e27b7c81128ec78dde6e21adeee4df41aa861
https://github.com/ <https://github.com/>
2021-08-26 14:00:24 -0300

    estrutura sobre criada

--- frontend/kd/src/app/app.module.ts
+++ frontend/kd/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
